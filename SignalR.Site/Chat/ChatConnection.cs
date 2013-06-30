6a4667a6e176c9ad8531c6532d5c0b36fe69bb3f
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-06-30 16:36:12 -0700

    Test commit from visual studio 2013.

--- SignalR.Site/Chat/ChatConnection.cs
+++ SignalR.Site/Chat/ChatConnection.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
 
 
 namespace SignalR.Chat
