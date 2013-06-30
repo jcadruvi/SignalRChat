e19c4686165537c005d9c6292c31c9d276b496bd
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-06-30 16:22:08 -0700

    Added ChatData.cs

---
+++ SignalR.Site/Chat/ChatData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Chat
+{
+    public class ChatData
+    {
+        public string Message { get; set; }
+        public string Name { get; set; }
+
+        public ChatData()
+        {
+
+        }
+
+        public ChatData(string name, string message)
+        {
+            Name = name;
+            Message = message;
+        }
+
+    }
+}
