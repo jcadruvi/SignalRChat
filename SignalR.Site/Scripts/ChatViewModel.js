f67038bd598cc6f6c512a4fb8c30d61bba2f2255
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-08-09 20:29:10 -0700

    Cleaned up the chatviewmodel javascript.

--- SignalR.Site/Scripts/ChatViewModel.js
+++ SignalR.Site/Scripts/ChatViewModel.js
@@ -1,6 +1,8 @@
 ﻿function chatViewModel() {
     self = {};
 
+    var _connection = null;
+
     self.content = ko.observable();
     self.content("");
     self.message = ko.observable();
@@ -8,30 +10,27 @@
     self.showChat = ko.observable();
     self.showName = ko.observable();
     self.showName(true);
-
-    var myConnection = $.connection("/chat");
-    myConnection.received(function (data) {
-        self.content(self.content() + ("<div>" + data.Name + ': ' + data.Message + "</div>"));
-    });
-
-    myConnection.error(function (error) {
-        console.warn(error);
-    });
-
-    myConnection.start()
-    .promise()
-    .done(function () {
-        $("#send").click(function () {
-            var myName = self.name();
-            var myMessage = self.message();
-            myConnection.send(JSON.stringify({ name: myName, message: myMessage }));
-            self.message(null);
-        });
-    });
+    
+    self.onSendClick = function () {
+        var myName = self.name();
+        var myMessage = self.message();
+        _connection.send(JSON.stringify({ name: myName, message: myMessage }));
+        self.message(null);
+    }
 
     self.onLogInClick = function () {
         self.showChat(true);
         self.showName(false);
+        _connection = $.connection("/chat");
+        _connection.received(function (data) {
+            self.content(self.content() + ("<div>" + data.Name + ': ' + data.Message + "</div>"));
+        });
+
+        _connection.error(function (error) {
+            console.warn(error);
+        });
+
+        _connection.start()
     };
 
     return self;
