215a1b779933fc7aa727a637423a006f28d93a49
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-08-23 19:37:52 -0700

    Adding the AddUser hub method.

--- SignalR.Model/User.cs
+++ SignalR.Model/User.cs
@@ -8,8 +8,15 @@ namespace SignalR.Model
 {
     public class User
     {
-        public int ConnectionId { get; set; }
+        public string ConnectionId { get; set; }
         public Guid Id { get; set; }
         public string Name { get; set; }
+
+        public User(string connectionId, Guid id, string name)
+        {
+            ConnectionId = connectionId;
+            Id = id;
+            Name = name;
+        }
     }
 }
--- SignalR.Site/Chat/ChatHub.cs
+++ SignalR.Site/Chat/ChatHub.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using SignalR.Services;
+using SignalR.Model;
 
 namespace SignalR.Chat
 {
     public class ChatHub : Hub
     {
+        private IUserService _userService;
+        
+        public ChatHub()
+        {
+            _userService = new UserService();
+        }
+
+        public void AddUser(string name)
+        {
+            _userService.AddUser(new User(Context.ConnectionId, Guid.NewGuid(), name));
+        }
+
         public void SendMessage(string name, string message)
         {
             Clients.All.sendMessage(name, message);
