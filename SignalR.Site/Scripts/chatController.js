70139117ed79f2700bbd78df07ffc38b0f531bfa
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-11-27 19:51:27 -0800

    Users list selects a user on click.

--- SignalR.Site/Scripts/chatController.js
+++ SignalR.Site/Scripts/chatController.js
@@ -1,15 +1,24 @@
 ﻿var chatApp = angular.module('chatApp', []);
 
 chatApp.controller('ChatController', function ($scope) {
+
     $scope.users = [
       {
           'name': 'John Doe',
+          'selected': true
       },
       {
-          'name': 'Jane Doe',
+          'name': 'Jane Doe'
       },
       {
-          'name': 'Fred Smith',
+          'name': 'Fred Smith'
       }
     ];
+
+    $scope.selectUser = function (user) {
+        for (var i = 0; i < $scope.users.length; i++) {
+            $scope.users[i].selected = false;
+        }
+        user.selected = true;
+    };
 });
