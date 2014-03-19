d214a9b73192617d28f24f0d2fbe87fa08f37bdf
Josh Cadruvi <jcadruvi78@yahoo.com>
2014-03-18 22:22:37 -0700

    Finished the signalRService.

--- SignalR.Site/Scripts/signalRService.js
+++ SignalR.Site/Scripts/signalRService.js
@@ -4,9 +4,19 @@
     angular.module('chatApp').factory('signalRService', signalRService);
 
     function signalRService() {
+
         var self = {};
 
+        self.connection = null;
+        self.proxy = null;
+
+        self.createConnection = function (hubName) {
+            connection = $.hubConnection();
+            proxy = connection.createHubProxy(hubName);
+            return connection.start();
+        };
 
         return self;
+
     }
 })();
